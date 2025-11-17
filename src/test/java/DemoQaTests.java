35ced40d26cd3862f6747ec95f666ab9835a7e22
Saten Asatryan <satenik.asat.qa@gmail.com>
2025-11-17 22:17:08 +0400

    Update .gitignore, DemoQaTests and build.gradle

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -3,44 +3,69 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DemoQaTests {
 
     @BeforeAll
-    static void beforAll(){
-        Configuration.browserSize="1980x1080";
-        Configuration.baseUrl="https://demoqa.com/";
-        Configuration.pageLoadStrategy="eager";
+    static void beforeAll() {
+        Configuration.browserSize = "1980x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 60000;
+
     }
 
     @Test
-    void fillInFileds(){
+    void fillInFields() {
 
         open("/automation-practice-form");
-        $("#firstName").setValue("Saten");
-        $("#lastName").setValue("Asatryan");
+
+
+
+        $("#firstName").shouldBe(visible).setValue("Ivana");
+        $("#lastName").setValue("Ivanova");
         $("#userEmail").setValue("tester@gmail.com");
         $(byText("Female")).click();
+        $("#userNumber").setValue("9876543210");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1980");
-        $("div.react-datepicker__day--06").click();
+        $(".react-datepicker__day--006:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Hiking");
+        $(byText("Reading")).click();
+        $("#currentAddress").setValue("Yerevan some street");
+        $("#uploadPicture").uploadFromClasspath("For DemoQaTests.jpeg");
+        $("#state").scrollIntoView(true).click();
+        $(byText("NCR")).shouldBe(visible).click();
+        $("#city").scrollIntoView(true).click();
+        $(byText("Noida")).shouldBe(visible).click();
+        $("#submit").click();
 
 
 
 
-       // $("#amount").setValue("1005");
-       // $("#category").setValue("Education_5");
-       // $("#save").click();
-      //  $("[placeholder=Search").setValue("Education_5").pressEnter();
-      //  $("tbody").$("tr").shouldHave(text("Education_5")).shouldHave(text("1005"));
+
+
+        $(".table-responsive table")
+                .shouldHave(text("Ivana"))
+                .shouldHave(text("Ivanova"))
+                .shouldHave(text("Female"))
+                .shouldHave(text("06 January,1980"))
+                .shouldHave(text("Hiking"))
+                .shouldHave(text("Reading"))
+                .shouldHave(text("Yerevan some street"))
+                .shouldHave(text("For DemoQaTests.jpeg"))
+                .shouldHave(text("NCR Noida"));
+
         sleep(5000L);
-      //  $("#submit").click();
-       // $("#userNumber").setValue("8978449393)
+
+
+
+
+
 
 
     }
 }
-
