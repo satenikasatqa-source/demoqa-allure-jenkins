03384930490c2819dcf625b74b89a1a1a0166bce
Satenik <satenik.asat.qa@gmail.com>
2026-01-21 12:17:05 +0300

    Fix demoqa project and attachments

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -1,5 +1,3 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,22 +5,12 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class DemoQaTests {
-
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1980x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.timeout = 60000;
-
-    }
+public class DemoQaTests extends TestBase {
 
     @Test
     void fillInFieldsTests() {
 
         open("/automation-practice-form");
-
-
 
         $("#firstName").shouldBe(visible).setValue("Ivana");
         $("#lastName").setValue("Ivanova");
@@ -43,10 +31,6 @@ public class DemoQaTests {
         $("#stateCity-wrapper").$(byText("Noida")).click();
         $("#submit").click();
 
-
-
-
-
         $(".table-responsive table")
                 .shouldHave(text("Ivana Ivanova"))
                 .shouldHave(text("tester@gmail.com"))
@@ -58,15 +42,5 @@ public class DemoQaTests {
                 .shouldHave(text("Yerevan some street"))
                 .shouldHave(text("ForDemoQaTests.jpeg"))
                 .shouldHave(text("NCR Noida"));
-
-
-
-
-
-
-
-
-
-
     }
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.clearProperty("selenide.baseUrl");
+        Configuration.baseUrl = "https://demoqa.com";
+        System.setProperty("selenide.baseUrl", "https://demoqa.com");
+
+        Configuration.browserSize = "1980x1080";
+        Configuration.timeout = 60000;
+
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
+    }
+
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        closeWebDriver();
+    }
 }
